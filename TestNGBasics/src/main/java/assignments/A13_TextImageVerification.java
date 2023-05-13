b4642f4c76fa6742ba725f4c8d714683fd68d9d1
chila <chila@AMBAL>
2023-05-13 15:57:48 +0530

    fifth commit

--- TestNGBasics/src/main/java/assignments/A13_TextImageVerification.java
+++ TestNGBasics/src/main/java/assignments/A13_TextImageVerification.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
@@ -15,25 +17,29 @@ import org.testng.asserts.SoftAssert;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class A13_TextImageVerification {
-	WebDriver driver = new ChromeDriver();
+	WebDriver driver;
 	SoftAssert softassert = new SoftAssert();
-	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
 	@BeforeMethod
 	public void launch_chrome() {
+		ChromeOptions co = new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*");
+		driver = new ChromeDriver(co);
 		WebDriverManager.chromedriver().setup();
 		driver.manage().window().maximize();
 		driver.get("https://selenium.obsqurazone.com/dynamic-load.php");
 	}
+
 	@Test
 	public void verify_newUserTextAndImageDisplay() {
 		WebElement getnewuser = driver.findElement(By.xpath("//button[@class='btn btn-info']"));
-		String expectedtext="Get New User";
-		String actualtext=getnewuser.getText();
+		String expectedtext = "Get New User";
+		String actualtext = getnewuser.getText();
 		softassert.assertEquals(actualtext, expectedtext);
 		getnewuser.click();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='card text-center']//img")));
-		WebElement image=driver.findElement(By.xpath("//div[@class='card text-center']//img"));
+		WebElement image = driver.findElement(By.xpath("//div[@class='card text-center']//img"));
 		softassert.assertTrue(image.isDisplayed());
 	}
 }
--- TestNGBasics/src/main/java/assignments/A14_Search.java
+++ TestNGBasics/src/main/java/assignments/A14_Search.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -14,30 +15,35 @@ import org.testng.asserts.SoftAssert;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class A14_Search {
-	WebDriver driver = new ChromeDriver();
+	WebDriver driver;
 	SoftAssert softassert = new SoftAssert();
+
 	@BeforeMethod
 	public void launch_chrome() {
+		ChromeOptions co = new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*");
+		driver = new ChromeDriver(co);
 		WebDriverManager.chromedriver().setup();
 		driver.manage().window().maximize();
 		driver.get("https://selenium.obsqurazone.com/table-sort-search.php");
 	}
+
 	@Test
 	public void verify_userDetails() {
-		WebElement searchfield=driver.findElement(By.xpath("//input[@type='search']"));
+		WebElement searchfield = driver.findElement(By.xpath("//input[@type='search']"));
 		searchfield.sendKeys("Ashton Cox");
-		List<String> expecteddetails=new ArrayList<String>();
+		List<String> expecteddetails = new ArrayList<String>();
 		expecteddetails.add("Ashton Cox");
 		expecteddetails.add("Junior Technical Author");
 		expecteddetails.add("San Francisco");
 		expecteddetails.add("66");
 		expecteddetails.add("2009/01/12");
 		expecteddetails.add("$86,000");
-		
-		List<WebElement> details=driver.findElements(By.xpath("//table//tbody//tr//td"));
-		for(int i=0;i<details.size();i++) {
-			String expected=expecteddetails.get(i);
-			String actual=details.get(i).getText();
+
+		List<WebElement> details = driver.findElements(By.xpath("//table//tbody//tr//td"));
+		for (int i = 0; i < details.size(); i++) {
+			String expected = expecteddetails.get(i);
+			String actual = details.get(i).getText();
 			softassert.assertEquals(actual, expected);
 		}
 	}
